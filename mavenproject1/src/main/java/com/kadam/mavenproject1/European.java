12c2b29909b053043055a456864e8e8ff30dbb3b
Krzysztof Adam <proceder7@gmail.com>
2017-06-25 17:45:05 +0200

    added European class

---
+++ mavenproject1/src/main/java/com/kadam/mavenproject1/European.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kadam.mavenproject1;
+
+/**
+ *
+ * @author kadam
+ */
+public class European implements Human{
+
+    @Override
+    public void info() {
+        System.out.println("I am the human - European");
+    }
+    
+    
+    
+}
